1af0f5539088d90a37b827590737bc54250f99b1
Sauddmamk <144232306+Sauddmamk@users.noreply.github.com>
2023-09-06 14:15:28 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+/*Programing code for a calculator.
+Programmer this code by Saud Saleh Alghamdi.
+*/
+int main() {
+    int x;
+    int y; 
+    
+    //First code, you have to put the first number
+    printf("number1:");
+    scanf("%d",&x);
+    
+    //Secondly, you have put the second number
+    printf("number2:");
+    scanf("%d",&y);
+    
+    //This code will calculate the addition operation
+    int s;
+    s=x+y;
+    printf("\nthe plural is: %d",s);
+    
+    //In this code, subtraction operations will be performed
+    int l;
+    l=x-y;
+    printf("\nthe substract is: %d",l);
+    
+    //In this code, division operations will be performed
+    int n;
+    n=x/y;
+    printf("\nthe divided is: %d",n);
+    
+    //In this code, multiplication operations will be performed
+    int d;
+    d=x*y;
+    printf("\nthe multiplied is: %d",d);
+
+
+
+
+} 
